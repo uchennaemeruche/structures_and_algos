464ca129192a8008ca7262d1675ba8f00b6f28ce
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-06-01 17:38:20 +0100

    updated relativeSortArray function

--- sort_array.js
+++ sort_array.js
@@ -21,19 +21,19 @@ Output: [22,28,8,6,17,44]
  */
 var relativeSortArray = function(arr1, arr2) {
 
-return arr1.sort(function (a, b) {
-    console.log("Values: ",a, b)
-    const index1 = arr2.indexOf(a);
-    const index2 = arr2.indexOf(b);
-    console.log("Index",index1, index2)
-    return ((index1 > -1 ? index1 : Infinity) - (index2 > -1 ? index2 : Infinity))
-//   return arr2.indexOf(a) - arr2.indexOf(b);
-})
+    return arr1.sort(function(a, b) {
+        console.log("Values: ", a, b)
+        const index1 = arr2.indexOf(a);
+        const index2 = arr2.indexOf(b);
+        console.log("Index", index1, index2)
+        return ((index1 > -1 ? index1 : Infinity) - (index2 > -1 ? index2 : Infinity))
+            //   return arr2.indexOf(a) - arr2.indexOf(b);
+    })
 
-  
+
 };
 
 
 
-console.log(relativeSortArray([28,6,22,8,44,17],[22,28,8,6]))
-console.log(relativeSortArray([2,3,1,3,2,4,6,7,9,2,19],[2,1,4,3,9,6]))
+console.log("Relative Array", relativeSortArray([28, 6, 22, 8, 44, 17], [22, 28, 8, 6]))
+console.log(relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], [2, 1, 4, 3, 9, 6]))
